63a6e9b0764276f81d69a5766b9fb86464b25426
AkmalKurbanov <250296a@gmail.com>
2019-10-15 04:22:12 +0600

    advantages section

--- src/js/scripts/chart.js
+++ src/js/scripts/chart.js
@@ -1,17 +1,43 @@
-$(function () {
-    $('.chart').easyPieChart({
-        barColor: '#00b7f4',
-        trackColor: '#b6b6b6',
-        scaleColor: false,
-        lineWidth: 4,
-        lineCap: 'circle',
-        size: 260,
-        animate:{
-            duration:2000,
-            enabled:true
-        },
-        onStep: function(from, to, percent) {
-            $(this.el).find('.count').text(Math.round(percent));
-        }
+$('.chart').each(function () {
+    var $chart = $(this),
+        size = parseFloat($chart.outerWidth()),
+        clearSet;
+
+    $chart.css({
+        height: size
+    })
+        .easyPieChart({
+            size: size,
+            animate: 2000,
+        });
+
+    $(window).on('resize', function () {
+
+        size = parseFloat($chart.outerWidth());
+
+        $chart.css({
+            height: size
+        });
+
+        $chart.removeData('easyPieChart').find('canvas').remove();
+        $chart.easyPieChart({
+            size: size,
+            animate: 1
+        });
     });
 });
+
+$(".total-js").each(function () {
+    $(this).prop("Counter", 0).animate({Counter: $(this).text()}, {
+        duration: 2000,
+        step: function (e) {
+            $(this).text(Math.ceil(e))
+        }
+    })
+});
+
+// $('.chart').attr('data-percent', total);
+// let number = parseInt($('.chart').children().find('.total-js').text());
+// let total = (number / 100);
+
+
