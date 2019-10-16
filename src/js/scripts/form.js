fd29a81af293bad88d92dc49de11673286acf99f
AkmalKurbanov <250296a@gmail.com>
2019-10-17 02:20:38 +0600

    news end

--- src/js/scripts/form.js
+++ src/js/scripts/form.js
@@ -2,8 +2,9 @@ var input = document.querySelector("#phone");
 window.intlTelInput(input, {
     initialCountry: "auto",
     nationalMode: true,
-    geoIpLookup: function(callback) {
-        $.get('https://ipinfo.io', function() {}, "jsonp").always(function(resp) {
+    geoIpLookup: function (callback) {
+        $.get('https://ipinfo.io', function () {
+        }, "jsonp").always(function (resp) {
             var countryCode = (resp && resp.country) ? resp.country : "";
             callback(countryCode);
         });
@@ -11,5 +12,17 @@ window.intlTelInput(input, {
 });
 
 
+$(document).on("click", ".registration-js", function () {
+    $('.modal-wrap').addClass('modal-window');
+    $('body').css('overflow-y', 'hidden');
+});
 
+$(document).on("click", ".close-btn", function () {
+    $('.modal-wrap').removeClass('modal-window');
+    $('body').css('overflow-y', 'auto');
+});
 
+$(document).on("click", ".registration-js", function () {
+    $('.mobile-menu').removeClass('open-menu');
+    $('.hamburger ').removeClass('hamburger-open');
+});
--- src/js/scripts/menu.js
+++ src/js/scripts/menu.js
@@ -1,8 +1,17 @@
 if ($(window).width() <= 767) {
     $('.header__nav-wrapper').clone().appendTo('.mobile-menu');
+    $('.hamburger').on('click', function () {
+        $(this).toggleClass('hamburger-open');
+        $('.mobile-menu').toggleClass('open-menu');
+        $('body').toggleClass('mobileBody-scroll');
+    });
 }
-$('.hamburger').on('click', function () {
-    $(this).toggleClass('hamburger-open');
-    $('.mobile-menu').toggleClass('open-menu');
-  });
+
+
+$('.nav__item').on('click', function () {
+$('.mobile-menu').removeClass('open-menu');
+$('body').removeClass('mobileBody-scroll');
+$('.hamburger ').removeClass('hamburger-open');
+});
+
 
