e68aa65c0bdeee38c04f6fdc13d076b4ea5067f5
AkmalKurbanov <250296a@gmail.com>
2019-10-21 21:22:21 +0600

    profile accordion sections

--- src/js/scripts/profile.js
+++ src/js/scripts/profile.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 
-    var readURL = function(input) {
+    var readURL = function (input) {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
 
@@ -14,11 +14,48 @@ $(document).ready(function() {
     }
 
 
-    $(".pr-img-upload").on('change', function(){
+    $(".pr-img-upload").on('change', function () {
         readURL(this);
     });
 
-    $(".pr-avatar-choose").on('click', function() {
+    $(".pr-avatar-choose").on('click', function () {
         $(".pr-img-upload").click();
     });
 });
+
+$('#search').on('keyup', function () {
+    var $this = $(this),
+        val = $this.val();
+
+    if (val.length >= 1) {
+        $('#search').css('max-width', '100%');
+        $('.delete-search').show();
+    } else {
+        $('#search').css('max-width', '192px');
+    }
+});
+
+
+$('.delete-search').on('click', function () {
+    $('#search').css('max-width', '192px').val('');
+    $(this).hide();
+});
+
+
+// select dropdown
+$('.select').on('click', function () {
+    $(this).children('.select__dropdown').slideToggle(300);
+});
+$('.select__option').on('click', function () {
+    let value = $(this).attr('data-value');
+    $('#select-type').val(value);
+    $('.select_checked').text(value);
+});
+$(document).mouseup(function (e) {
+    let div = $(".select");
+    if (!div.is(e.target)
+        && div.has(e.target).length === 0) {
+        $('.select__dropdown').slideUp();
+    }
+});
+// select dropdown end
