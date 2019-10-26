1bf945a0d8d5377fff9ed721b8d442fba777b239
AkmalKurbanov <250296a@gmail.com>
2019-10-26 21:07:51 +0600

    pyramid profile vip

--- src/js/scripts/chart.js
+++
@@ -1,9 +0,0 @@
-$(".total-js").each(function () {
-    $(this).prop("Counter", 0).animate({Counter: $(this).text()}, {
-        duration: 2000,
-        easing: "swing",
-        step: function (e) {
-            $(this).text(Math.ceil(e))
-        }
-    })
-});
